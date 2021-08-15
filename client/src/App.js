d48fdb6a608a2cb5f325c3b55792c1e44c6159da
Dirk Kinley <dkinley@gmail.com>
2021-08-15 16:04:53 -0700

    updates to css flex

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import GetAll from './components/AllProfiles';
 import New from './components/CreateProfile';
 import Edit from './components/EditProfile';
 import Details from './components/ProfileDetails';
+import Header from './components/Header';
 import { Router } from '@reach/router'; // since router is not exported as default {} are required
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Router>
         <GetAll path="/" />
         <New path="/profile/new" />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { Link, navigate } from '@reach/router';
+import axios from 'axios';
+//import { Link, navigate } from '@reach/router';
+    
+    const Header = (props) => {
+        const [ profile, setProfile ] = useState({});
+        
+        useEffect(() => {
+            axios.get('http://localhost:8000/api/profile/' + props.id) // postman verified url
+                .then((res) => {
+                    console.log(res.data); //.then gives response object which is commonly referred to as res
+                    setProfile(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            }, [props.id ]);
 
-const Header = (props) => {
     return (
             <header>
-                <img src="../leaderBoard4.png"/>
+            <div>{profile.firstName} {profile.lastName}</div>
             </header>
-    )
+        )
 };
 export default Header;
